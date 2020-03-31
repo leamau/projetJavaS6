e74fa3f3533711e5401ebf996ef84fc50ac83de0
Maël Hingouët <mael.hingouet@hotmail.fr>
2020-03-31 09:03:10 +0200

    Test commit

--- src/main/java/org/app/Usine.java
+++ src/main/java/org/app/Usine.java
@@ -4,6 +4,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Usine {
+
+    // Attributs.
+
+    /**
+     * Les éléments stockés dans l'usine.
+     */
     private HashSet<Element> elements;
+
+    /**
+     * Les chaînes de production disponibles.
+     */
     private HashSet<Chaine>  chaines;
 }
